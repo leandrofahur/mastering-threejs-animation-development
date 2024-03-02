89725b75eeac7397c3969e1493d8d03fde5357c7
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-03-02 10:47:06 -0800

    my first program

--- modules/01-basics/main.js
+++ modules/01-basics/main.js
@@ -1,42 +1,51 @@
 import * as THREE from "three";
 
-// Define a Scene, Camera and Renderer
+// 1st: create a scene:
 const scene = new THREE.Scene();
 
+// 2nd: create a camera and set its position and add it to the scene:
 const sizes = {
-  width: 800,
-  height: 600,
+  width: 600,
+  height: 400,
 };
+
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
   1000
 );
+camera.position.z = 3;
+scene.add(camera);
 
-// Define a geometry and a material
+// 3rd: create a object = geometry + material and add it to the scene:
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const material = new THREE.MeshBasicMaterial({ color: "#0000f0" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-// Add the mesh to the scene
-scene.add(cube);
-
-// Position the camera
-camera.position.z = 5;
-
-// Set the renderer size and add it to the DOM
+// 4th: create a renderer:
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement); // add the renderer to the body
 
-function animate() {
-  requestAnimationFrame(animate);
+// 5th: render the scene:
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+// 6th: create an animation loop:
+const clock = new THREE.Clock();
+const animate = () => {
+  const elapsedTime = clock.getElapsedTime();
 
+  // update objects
+  mesh.rotation.y = elapsedTime;
+  mesh.rotation.x = elapsedTime;
+
+  // render the scene
   renderer.render(scene, camera);
-}
+
+  // call tick again on the next frame
+  window.requestAnimationFrame(animate);
+};
 
 animate();
