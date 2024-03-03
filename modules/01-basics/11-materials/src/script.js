623df8088dd9a38e6bf2530e6c7d7da613cc0a97
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-03-02 17:21:03 -0800

    fulling around

--- modules/01-basics/11-materials/src/script.js
+++ modules/01-basics/11-materials/src/script.js
@@ -8,9 +8,29 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const axesHelper = new THREE.AxesHelper();
+// const axesHelper = new THREE.AxesHelper();
 const scene = new THREE.Scene();
-scene.add(axesHelper);
+// scene.add(axesHelper);
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
+const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const doorAmbientOcclusionTexture = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const doorHeightTexture = textureLoader.load("/textures/door/height.jpg");
+const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
+const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+
+const matcapTexture = textureLoader.load("/textures/matcaps/1.png");
+const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
+
+doorColorTexture.encoding = THREE.sRGBEncoding;
+matcapTexture.encoding = THREE.sRGBEncoding;
 
 /**
  * Sizes
@@ -54,6 +74,39 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 /**
+ * Objects
+ */
+// Mesh Basic Material
+// const material = new THREE.MeshBasicMaterial();
+
+// material.map = doorColorTexture;
+// material.alphaMap = doorAlphaTexture;
+// material.transparent = true;
+// material.side = THREE.DoubleSide;
+
+const material = new THREE.MeshNormalMaterial();
+
+const sphereAxesHelper = new THREE.AxesHelper();
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+sphere.position.x = -1.5;
+sphere.add(sphereAxesHelper);
+
+const planeAxesHelper = new THREE.AxesHelper();
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material);
+plane.rotation.x = -Math.PI * 0.5;
+plane.add(planeAxesHelper);
+
+const torusAxesHelper = new THREE.AxesHelper();
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  material
+);
+torus.position.x = 1.5;
+torus.add(torusAxesHelper);
+
+scene.add(sphere, plane, torus);
+
+/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
@@ -71,6 +124,10 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update controls
+  sphere.rotation.y = elapsedTime * 0.1;
+  plane.rotation.y = elapsedTime * 0.1;
+  torus.rotation.y = elapsedTime * 0.1;
+
   controls.update();
 
   // Render
